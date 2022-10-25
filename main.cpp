1535775f404e52f1dff5680e26910321edeab117
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-25 00:41:56 +0000

    basic matrix input and printing

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  const int N_ROWS = 2;
+  const int N_COLS = 5;
+  double gradeMatrix[N_ROWS][N_COLS];
+
+  // user input of matrix values
+  cout << "Enter the value of the matrix: " << endl;
+  for(int r = 0; r < N_ROWS; r++){  
+    for (int c = 0; c < N_COLS; c++){
+      cin >> gradeMatrix[r][c];
+      cout << endl;
+    }
+  }
+
+  // print matrix
+  cout << "The values of the matrix are: " << endl;
+  for(int r = 0; r < N_ROWS; r++){  
+    for (int c = 0; c < N_COLS; c++){
+      cout << gradeMatrix[r][c];
+      
+    }
+  }
+}
